5055cc439aa000814cd03c3aaadc35695817e961
monikasa <monika.sawicka1988@gmail.com>
2022-06-13 15:21:58 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+
+/* Looping through images */
+for (const image of images) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/${image}`);
+  thumbBar.appendChild(newImage);
+  newImage.addEventListener('click', e => displayedImage.src = e.target.src);
+}
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
