b2b0d702bf219045009a9d6ac62b227340725e32
monikasa <monika.sawicka1988@gmail.com>
2022-06-13 15:38:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
 /* Looping through images */
 for (const image of images) {
   const newImage = document.createElement('img');
-  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('src', `ImageGallery/Images/${image}`);
   thumbBar.appendChild(newImage);
   newImage.addEventListener('click', e => displayedImage.src = e.target.src);
 }
